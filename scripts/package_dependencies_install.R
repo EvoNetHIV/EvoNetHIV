3ac4ae69cd1816d1bd5c5f7d3319ae774c1d4e00
unknown <jtm6@uw.edu>
2018-03-07 22:33:32 -0800

    install help

--- scripts/package_dependencies_install.R
+++ scripts/package_dependencies_install.R
@@ -1,3 +1,4 @@
+#############################################
 #When working with evonet as RStudio project, may need to 
 #install the following packages when using new computer or 
 #if R on CSDE server was updated.
@@ -10,3 +11,13 @@ install.packages("plotrix")
 install.packages("testthat")
 devtools::install_github( "statnet/tergmLite",force=T)
 devtools::install_github( "statnet/EpiModel", ref ="fast_edgelist")
+
+##########################################
+#install evonet as a regular package 
+
+if (!require("devtools")) {
+  install.packages("devtools")
+  library(devtools)}
+install_github("EvoNetHIV/EvoNet",subdir="pkg")
+library(evonet)
+##########################################
