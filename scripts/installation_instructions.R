57dd11021907061f60502e1380aa861832db2b76
unknown <jtm6@uw.edu>
2018-04-02 09:39:53 -0700

    edits to installation instructions

--- scripts/installation_instructions.R
+++ scripts/installation_instructions.R
@@ -44,5 +44,7 @@ devtools::install_github( "statnet/EpiModel", ref ="fast_edgelist")
 
 ##########################################
 #install absdiffby branch as package
-
+library(devtools)
+devtools::install_github( "statnet/tergmLite",force=T)
+devtools::install_github( "statnet/EpiModel", ref ="fast_edgelist",force=T)
 install_github("EvoNetHIV/EvoNet",subdir="pkg",ref="absdiffby",force=TRUE)
